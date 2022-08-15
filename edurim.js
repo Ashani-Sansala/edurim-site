3bdb856af4d2144795f470f74431ad309c6f5f6f
Ashani-Sansala <ashani.sansala16@gmail.com>
2022-08-15 16:06:57 +0530

    commit files

---
+++ edurim.js
@@ -0,0 +1,14 @@
+
+function autoScroll(){
+    window.scrollTo({
+        top: 650,
+        behavior: "smooth",
+    });
+}
+
+function autoScroll2(){
+    window.scrollTo({
+        top: 5050,
+        behavior: "smooth",
+    });
+}
